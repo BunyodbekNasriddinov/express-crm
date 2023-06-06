12cf6cddb1ab05755052529a6720aca31e91f089
BunyodbekNasriddinov <nasriddinoffbunydobek@gmail.com>
2023-06-06 22:21:41 +0500

    jwt.js add and setup

---
+++ src/utils/jwt.js
@@ -0,0 +1,9 @@
+import jwt from "jsonwebtoken";
+import "dotenv/config";
+
+const SECRET_KEY = process.env.SECRET_KEY;
+
+export default {
+  sign: (payload) => jwt.sign(payload, SECRET_KEY, { expiresIn: "1w" }),
+  verify: (token) => jwt.verify(token, SECRET_KEY),
+};
