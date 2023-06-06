0d723dd1b1a04cfc12f1de17f11121dbf9239989
BunyodbekNasriddinov <nasriddinoffbunydobek@gmail.com>
2023-06-06 23:19:13 +0500

    cors fixed

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5001;
 const app = express();
 
 // setup
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 app.use(express.static(resolve("uploads")));
 
