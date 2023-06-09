79b6981a48027145881c904691d159d08f3e7b93
BunyodbekNasriddinov <nasriddinoffbunydobek@gmail.com>
2023-06-09 23:07:20 +0500

    adminRoute export

--- src/routes/admins.routes.js
+++ src/routes/admins.routes.js
@@ -4,4 +4,6 @@ import { ADMIN_LOGIN } from "../controllers/admins.controller.js";
 const router = Router();
 
 
-router.post('/admin/login', ADMIN_LOGIN)
+router.post('/admin/login', ADMIN_LOGIN);
+
+export default router;
