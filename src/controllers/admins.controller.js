7d76db6f76ae53d8db7079ed4fb029c12422cdaa
BunyodbekNasriddinov <nasriddinoffbunydobek@gmail.com>
2023-06-09 23:06:42 +0500

    adminLogin add

--- src/controllers/admins.controller.js
+++ src/controllers/admins.controller.js
@@ -1,3 +1,22 @@
+import { BadRequestError, InternalServerError } from "../errors/errors.js";
+import { adminLoginModel } from "../models/admin/admins.model.js";
+import jwt from "../utils/jwt.js";
+
 export const ADMIN_LOGIN = async (req, res, next) => {
-  const newAdmin = await 1;
-} 
+  const admin = await adminLoginModel(req.body);
+
+  try {
+    if (!admin)
+      return next(new BadRequestError("Invalid username or password"));
+
+    const token = jwt.sign(admin.admin_id);
+    admin.password = null;
+    console.log(admin);
+
+    res
+      .status(200)
+      .json({ status: 200, message: "successfully login", token, data: admin });
+  } catch (error) {
+    return next(new InternalServerError(error.message));
+  }
+};
