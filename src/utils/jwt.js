67a56a64655a9ad558ca7ea5ea4c014ef556d503
BunyodbekNasriddinov <nasriddinoffbunydobek@gmail.com>
2023-06-09 23:06:14 +0500

    jwt expiresIn fixed

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -4,6 +4,6 @@ import "dotenv/config";
 const SECRET_KEY = process.env.SECRET_KEY;
 
 export default {
-  sign: (payload) => jwt.sign(payload, SECRET_KEY, { expiresIn: "1w" }),
+  sign: (payload) => jwt.sign(payload, SECRET_KEY),
   verify: (token) => jwt.verify(token, SECRET_KEY),
 };
