dded76293e4b01c4a4a5a8b04ca1a4b68d79d5e6
BunyodbekNasriddinov <nasriddinoffbunydobek@gmail.com>
2023-06-09 23:06:57 +0500

    adminRoute add

--- src/routes/all.routes.js
+++ src/routes/all.routes.js
@@ -3,7 +3,11 @@ import { Router } from "express";
 // swagger import
 import swaggerRoute from "../utils/swagger.js";
 
+import adminRoutes from "../routes/admins.routes.js";
+
 const router = Router();
+
+router.use(adminRoutes);
 
 router.use(swaggerRoute);
 
