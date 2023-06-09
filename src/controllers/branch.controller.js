7c9368d8d62f24522c575cfd42e3a636f344d98c
BunyodbekNasriddinov <nasriddinoffbunydobek@gmail.com>
2023-06-10 00:13:10 +0500

    branch queryFilter add

--- src/controllers/branch.controller.js
+++ src/controllers/branch.controller.js
@@ -2,10 +2,14 @@ import {
   allBramchModel,
   createBrachModel,
 } from "../models/branch/branch.model.js";
+import { queryFilter } from "../utils/model.js";
 
 export const ALL_BRANCH = async (req, res, next) => {
-  const branches = await allBramchModel();
-  console.log(branches);
+  let branches = await allBramchModel();
+
+  branches = queryFilter(req.query, branches);
+
+  res.status(200).json({ status: 200, message: "ok", data: branches });
 };
 
 export const NEW_BRANCH = async (req, res, next) => {
